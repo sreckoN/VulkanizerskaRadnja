69786ad6186a1ecec5c3f5a531c20052332a6020
sreckoN <srecko.nikolic71@hotmail.com>
2018-03-24 15:37:13 +0100

    Ispravljene greske u kodu otkrivene testiranjem

--- src/gume/AutoGuma.java
+++ src/gume/AutoGuma.java
@@ -63,7 +63,7 @@ public class AutoGuma {
 	 * @throws java.lang.RuntimeException kada je precnik manji od 13 ili veci od 22
 	 */
 	public void setPrecnik(int precnik) {
-		if (precnik < 13 && precnik > 22)
+		if (precnik < 13 || precnik > 22)
 			throw new RuntimeException("Precnik van opsega");
 		this.precnik = precnik;
 	}
@@ -82,7 +82,7 @@ public class AutoGuma {
 	 * @throws java.lang.RuntimeException kada je sirina manja od 135 ili veca od 355
 	 */
 	public void setSirina(int sirina) {
-		if (sirina < 135 && sirina > 355)
+		if (sirina < 135 || sirina > 355)
 			throw new RuntimeException("Sirina van opsega");
 		this.sirina = sirina;
 	}
